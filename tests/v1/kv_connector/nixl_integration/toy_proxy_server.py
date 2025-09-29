8c2ffeceaa5fd7f52fc456987ca548fdc7608ce8
Yunzhao <liu3882@purdue.edu>
2025-09-29 23:11:35 +0000

    frontend fix on decode side too

--- tests/v1/kv_connector/nixl_integration/toy_proxy_server.py
+++ tests/v1/kv_connector/nixl_integration/toy_proxy_server.py
@@ -44,7 +44,7 @@ async def lifespan(app: FastAPI):
         decoder_base_url = f'http://{host}:{port}/v1'
         app.state.decode_clients.append({
             'client':
-            httpx.AsyncClient(timeout=None, base_url=decoder_base_url),
+            httpx.AsyncClient(timeout=None, base_url=decoder_base_url, limits=httpx.Limits(max_keepalive_connections=512, max_connections=512)),
             'host':
             host,
             'port':
