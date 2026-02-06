8cf3404454458a260721a0cb858bb58fba83bb8b
OmarBasit1 <omar.basit@gmail.com>
2026-02-06 23:55:06 +0000

    specifically added token budget used as well, note only works if asyn schedulign is off, otherwise it may be off by 1 (sometimes)

--- vllm/v1/core/sched/scheduler.py
+++ vllm/v1/core/sched/scheduler.py
@@ -129,6 +129,7 @@ class Scheduler(SchedulerInterface):
         self.running: list[Request] = []
         self.last_running_len = 0
         self.last_waiting_len = 0
+        self.last_token_budget_used = 0
 
         # The request IDs that are finished in between the previous and the
         # current steps. This is used to notify the workers about the finished
@@ -580,6 +581,7 @@ class Scheduler(SchedulerInterface):
             self.last_running_len = len(scheduled_new_reqs) + len(scheduled_resumed_reqs) + \
                                     len(scheduled_running_reqs)
             self.last_waiting_len = len(self.waiting)
+            self.last_token_budget_used = self.max_num_scheduled_tokens - token_budget
 
         # Construct the scheduler output.
         new_reqs_data = [
@@ -1271,6 +1273,7 @@ class Scheduler(SchedulerInterface):
         return SchedulerStats(
             num_running_reqs=self.last_running_len,
             num_waiting_reqs=self.last_waiting_len,
+            token_budget_used=self.last_token_budget_used,
             kv_cache_usage=self.kv_cache_manager.usage,
             prefix_cache_stats=prefix_cache_stats,
             spec_decoding_stats=spec_decoding_stats,
--- vllm/v1/metrics/stats.py
+++ vllm/v1/metrics/stats.py
@@ -47,14 +47,7 @@ class SchedulerStats:
     now: float = 0.0
     num_running_reqs: int = 0
     num_waiting_reqs: int = 0
-    waiting_reqs_num_tokens: list[int] = field(default_factory=list)
-    running_reqs_num_tokens: list[int] = field(default_factory=list)
-    waiting_reqs_num_time: list[float] = field(default_factory=list)
-    running_reqs_num_time: list[float] = field(default_factory=list)
-    running_computed_tokens_list: list[int] = field(default_factory=list)
-    waiting_computed_tokens_list: list[int] = field(default_factory=list)
-    running_computed_req_list: list[str] = field(default_factory=list)
-    waiting_computed_req_list: list[str] = field(default_factory=list)
+    token_budget_used: int = 0
 
     # These are used for internal DP load-balancing.
     step_counter: int = 0
